e4bdc76b55d364a8ef2e85e7a62a37133023cb60
Deniz Sarıkaş <75947870+denizsarikas@users.noreply.github.com>
2022-11-27 20:54:34 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./styles.css"
+import React, {useState} from "react";
+function App() {
+
+  const [minVal, setMinVal] = useState(0);
+  const [maxVal, setMaxVal] = useState(10);
+  const [randomVal, setRandomVal] = useState(5);
+
+  const giveRandomNumber=() => {
+    setRandomVal(Math.floor(Math.random() * (maxVal - minVal) ) + minVal);
+
+  }
+  return (
+    <div className="App">
+        <h1>random number generator</h1>
+        <div className="container">
+          <div className="randomNum">
+            <p> Random Number : <span>{randomVal}</span></p>
+          </div>
+
+          <div className="numContainer">
+            <div>
+              <p>Min:</p>
+              <input
+              type="number"
+              value={minVal}
+              onChange={ e => setMinVal(+e.target.value)}
+               />
+            </div>
+            <div>
+              <p>Max:</p>
+              <input
+              type="number"
+              value={maxVal}
+              onChange={ e => setMaxVal(+e.target.value)}
+               />
+            </div>
+          </div>
+        <button onClick={giveRandomNumber}>Get Random Number</button>
+        </div>
+    </div>
+  );
+}
+
+export default App;
